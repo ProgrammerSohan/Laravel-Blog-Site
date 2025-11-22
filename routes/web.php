82e9c937510dffd9d842e02086c2c9648dbe95f4
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-11-23 02:24:15 +0600

    2.2-managing routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * ADMIN ROUTES
+ * 
+ */
+Route::prefix('admin')->name('admin.')->group(function(){
+    Route::middleware([])->group(function(){
+        Route::controller(AuthController::class)->group(function(){
+            Route::get('/login','loginForm')->name('login');
+            Route::get('/forgot-password','forgotForm')->name('forgot');
+        });
+    });
+
+    Route::middleware([])->group(function(){
+        Route::controller(AdminController::class)->group(function(){
+            Route::get('/dashboard','adminDashboard')->name('dashboard');
+        });
+
+    });
+
+});
+
+
+
