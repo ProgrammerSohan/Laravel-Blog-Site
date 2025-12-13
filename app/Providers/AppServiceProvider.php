6596eee8b76d183a9211672f95171e7a1559fe68
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-12-13 06:13:08 +0600

    05-secure admin routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Session;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         //Redirect an authenticated user to dashboard
+        RedirectIfAuthenticated::redirectUsing(function(){
+            return route('admin.dashboard');
+        });
+
+        //Redirect No Authenticated user to admin login page
+        Authenticate::redirectUsing(function(){
+            Session::flash('fail','You must be logged in to access admin area.Please login to continue.');
+            return route('admin.login');
+
+        });
     }
 }
